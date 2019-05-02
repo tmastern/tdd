e561c735ec78360d79d2ffe1981e41888066b1ee
tmastern <tmastern@gmail.com>
2019-05-02 14:35:18 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -15,7 +17,33 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail("Finish the test!")
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		# She is invited to enter a to-do item straght away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+
+		# She types her item into a text box
+		input.send_keys('Buy peacock feathers')
+
+		# When she hits enter, the page updates with new item
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers for row in rows')
+		)
+
+		# There is still a box inviting enter next item
+		self.fail('Finish the Test!')
+
+		# The page updates again and shows both items 
 
 
 
@@ -23,15 +51,6 @@ class NewVisitorTest(unittest.TestCase):
 
 
 
-
-		# She is invited to enter a to-do item right away
-
-		# She types "Buy peacock feathers" into a text box
-
-		# When she hits enter, the page updates.
-		# "1: Buy peacock feathers" is added to the list
-
-		# There is still a text box inviting another item
 
 		# The page updates again to show two items on list
 
